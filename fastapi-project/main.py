f22a993ac2d5f242f128f138f42dc053424be5c2
Adrihp06 <adrianhdezp10@gmail.com>
2023-08-06 19:30:33 +0100

    drink class

--- fastapi-project/main.py
+++ fastapi-project/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
